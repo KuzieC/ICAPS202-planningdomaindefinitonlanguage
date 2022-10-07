ee94555c321e03236c3660552a220324acb20d3c
Guanzhi Chen <99990014+KuzieC@users.noreply.github.com>
2022-10-07 11:56:39 +0100

    Update mainwindow.h

--- klostki2/mainwindow.h
+++ klostki2/mainwindow.h
@@ -16,8 +16,8 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
-    QList<QString> Commandlist;//不能用list这样的变量名字 ，list是c++的一个类型，重复。而且在QT，优先用qt的类，QList
-    QMap<QString , QPushButton*> mapList;//QMap头文件都没有include，你怎么编译过去的。
+    QList<QString> Commandlist;
+    QMap<QString , QPushButton*> mapList;
     QMap<QString,int> coordListH;
     QMap<QString,int> coordListV;
     int counter = 0;
